904a9214ea76fac231d199238952455b896f2f58
John Furrow <hello@johnfurrow.com>
2017-09-01 22:36:26 -0700

    Allow clicks on FormElementAddon elements

--- src/components/FormElementAddon.js
+++ src/components/FormElementAddon.js
@@ -5,6 +5,7 @@ import React, {Component} from 'react';
 export default class FormElementAddon extends Component {
   static propTypes = {
     addonPlacement: PropTypes.oneOf(['before', 'after']),
+    onClick: PropTypes.func,
     type: PropTypes.oneOf(['icon']),
     isInteractive: PropTypes.bool
   };
@@ -19,14 +20,15 @@ export default class FormElementAddon extends Component {
       'form__element__addon',
       {
         [`form__element__addon--placed-${this.props.addonPlacement}`]: this.props.addonPlacement,
-        'form__element__addon--is-interactive': this.props.isInteractive,
+        [`form__element__addon--index-${this.props.addonIndex}`]: this.props.addonIndex,
+        'form__element__addon--is-interactive': this.props.isInteractive || this.props.onClick,
         'form__element__addon--is-icon': this.props.type === 'icon'
       },
       this.props.className
     );
 
     return (
-      <div className={classes}>
+      <div className={classes} onClick={this.props.onClick}>
         {this.props.children}
       </div>
     );
